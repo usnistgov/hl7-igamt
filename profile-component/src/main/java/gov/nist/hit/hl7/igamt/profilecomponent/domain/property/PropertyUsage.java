e63a62bcfb5be5abc1a342bd0d9b6ff7b9556127
Lefort, Maxence C <maxence.lefort@nist.gov>
2018-02-21 11:53:00 -0500

    Add more ItemProperty objects

---
+++ profile-component/src/main/java/gov/nist/hit/hl7/igamt/profilecomponent/domain/property/PropertyUsage.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ * This software was developed at the National Institute of Standards and Technology by employees of
+ * the Federal Government in the course of their official duties. Pursuant to title 17 Section 105
+ * of the United States Code this software is not subject to copyright protection and is in the
+ * public domain. This is an experimental system. NIST assumes no responsibility whatsoever for its
+ * use by other parties, and makes no guarantees, expressed or implied, about its quality,
+ * reliability, or any other characteristic. We would appreciate acknowledgement if the software is
+ * used. This software can be redistributed and/or modified freely provided that any derivative
+ * works bear some notice that they are derived from it, and any modified versions bear some notice
+ * that they have been modified.
+ * 
+ */
+package gov.nist.hit.hl7.igamt.profilecomponent.domain.property;
+
+import gov.nist.hit.hl7.igamt.shared.domain.Usage;
+
+/**
+ *
+ * @author Maxence Lefort on Feb 21, 2018.
+ */
+public class PropertyUsage extends ItemProperty {
+
+  private Usage usage;
+
+  public PropertyUsage(Usage usage) {
+    super(PropertyKey.USAGE);
+    this.usage = usage;
+  }
+
+  public Usage getUsage() {
+    return usage;
+  }
+
+  public void setUsage(Usage usage) {
+    this.usage = usage;
+  }
+
+}
