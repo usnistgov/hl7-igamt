52c490cc4799b485cee607f917f8b546f38cdf62
Lefort, Maxence C <maxence.lefort@nist.gov>
2018-03-29 15:47:50 -0400

    Add conformance profile serialization

--- domain/src/main/java/gov/nist/hit/hl7/igamt/shared/domain/Group.java
+++ domain/src/main/java/gov/nist/hit/hl7/igamt/shared/domain/Group.java
@@ -7,7 +7,6 @@ import gov.nist.hit.hl7.igamt.shared.domain.binding.ResourceBinding;
 
 public class Group extends MsgStructElement {
 
-  private String name;
   private Set<MsgStructElement> children = new HashSet<MsgStructElement>();
   private ResourceBinding binding;
 
@@ -17,20 +16,19 @@ public class Group extends MsgStructElement {
     this.setType(Type.GROUP);
   }
 
+  public Group(String id, String name, int position, Usage usage, String text,
+      boolean custom, int min, String max, Set<MsgStructElement> children, ResourceBinding binding) {
+    super(id, name, position, usage, Type.GROUP, text, custom, min, max);
+    this.children = children;
+    this.binding = binding;
+  }
+
   public Set<MsgStructElement> getChildren() {
     return children;
   }
 
   public void setChildren(Set<MsgStructElement> children) {
     this.children = children;
-  }
-
-  public String getName() {
-    return name;
-  }
-
-  public void setName(String name) {
-    this.name = name;
   }
 
   public ResourceBinding getBinding() {
