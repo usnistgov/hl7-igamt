f2f9ec03b07f48bfd11858862fcd42d00352a7e0
Lefort, Maxence C <maxence.lefort@nist.gov>
2018-04-19 16:47:13 -0400

    Add new model for client side DB

--- client/src/app/service/indexed-db/indexed-db.service.ts
+++ client/src/app/service/indexed-db/indexed-db.service.ts
@@ -1,222 +1,107 @@
 import {Injectable} from '@angular/core';
 
-import { ObjectsDatabase } from './objects-database';
-import { DatatypesService } from '../datatypes/datatypes.service';
-import { ValueSetsService } from '../valueSets/valueSets.service';
-import {SegmentsService} from "../segments/segments.service";
-import {ProfileComponentsService} from "../profilecomponents/profilecomponents.service";
+import { ObjectsDatabase, IObject } from './objects-database';
+import { ObjectsReferenceDatabase } from './objects-reference-database';
+import {isUndefined} from 'ngx-bootstrap/bs-moment/utils/type-checks';
 
 @Injectable()
 export class IndexedDbService {
-
-  objectsDatabase;
   changedObjectsDatabase;
-  constructor(private datatypesService: DatatypesService, private valueSetsService: ValueSetsService, private segmentsService: SegmentsService, private profileComponentsService: ProfileComponentsService) {
-    this.objectsDatabase = new ObjectsDatabase('ObjectsDatabase');
+  removedObjectsDatabase;
+  createdObjectsDatabase;
+  addedObjectsDatabase;
+  igDocumentId?: string;
+  constructor(igDocumentId) {
     this.changedObjectsDatabase = new ObjectsDatabase('ChangedObjectsDatabase');
+    this.removedObjectsDatabase = new ObjectsReferenceDatabase('RemovedObjectsDatabase');
+    this.createdObjectsDatabase = new ObjectsReferenceDatabase('CreatedObjectsDatabase');
+    this.addedObjectsDatabase = new ObjectsReferenceDatabase('AddedObjectsDatabase');
+    this.igDocumentId = igDocumentId;
   }
-  public init(ig) {
-    this.objectsDatabase.transaction('rw', this.objectsDatabase.datatypes, async() => {
-      this.objectsDatabase.datatypes.clear().then(this.injectDatatypes(ig.profile.datatypeLibrary.id));
+  public init() {
+    this.changedObjectsDatabase.transaction('rw', this.changedObjectsDatabase, async() => {
+       this.changedObjectsDatabase.clear();
     });
-    this.changedObjectsDatabase.transaction('rw', this.changedObjectsDatabase.datatypes, async() => {
-       this.changedObjectsDatabase.datatypes.clear();
+    this.removedObjectsDatabase.transaction('rw', this.removedObjectsDatabase, async() => {
+       this.removedObjectsDatabase.clear();
     });
-    // valueSets
-    this.objectsDatabase.transaction('rw', this.objectsDatabase.valueSets, async() => {
-      this.objectsDatabase.valueSets.clear().then(this.injectValueSets(ig.profile.tableLibrary.id));
+    this.createdObjectsDatabase.transaction('rw', this.createdObjectsDatabase, async() => {
+       this.createdObjectsDatabase.clear();
     });
-    this.changedObjectsDatabase.transaction('rw', this.changedObjectsDatabase.valueSets, async() => {
-       this.changedObjectsDatabase.valueSets.clear();
-    });
-    // segments
-    this.objectsDatabase.transaction('rw', this.objectsDatabase.segments, async() => {
-      this.objectsDatabase.segments.clear().then(this.injectSegments(ig.profile.segmentLibrary.id));
-    });
-    this.changedObjectsDatabase.transaction('rw', this.changedObjectsDatabase.segments, async() => {
-       this.changedObjectsDatabase.segments.clear();
-    });
-    // sections
-    this.objectsDatabase.transaction('rw', this.objectsDatabase.sections, async() => {
-     // this.objectsDatabase.sections.clear().then(this.injectDatatypes(igDocumentId));
-    });
-    this.changedObjectsDatabase.transaction('rw', this.changedObjectsDatabase.sections, async() => {
-      // this.changedObjectsDatabase.sections.clear();
-    });
-    // profileComponents
-    this.objectsDatabase.transaction('rw', this.objectsDatabase.profileComponents, async() => {
-      this.objectsDatabase.profileComponents.clear().then(this.injectProfileComponents(ig.profile.profileComponentLibrary.id));
-    });
-    this.changedObjectsDatabase.transaction('rw', this.changedObjectsDatabase.profileComponents, async() => {
-      // this.changedObjectsDatabase.profileComponents.clear();
-    });
-    // profiles
-    this.objectsDatabase.transaction('rw', this.objectsDatabase.profiles, async() => {
-      // this.objectsDatabase.profiles.clear().then(this.injectDatatypes(igDocumentId));
-    });
-    this.changedObjectsDatabase.transaction('rw', this.changedObjectsDatabase.profiles, async() => {
-      // this.changedObjectsDatabase.profiles.clear();
+    this.addedObjectsDatabase.transaction('rw', this.addedObjectsDatabase, async() => {
+      this.addedObjectsDatabase.clear();
     });
   }
 
-  /*
-  .filter(function (fullDatatype) {
-        const metadataDatatype = {
-          'label': fullDatatype.label,
-          'hl7Version': fullDatatype.hl7Version
-        }
-        return metadataDatatype;
-      });
-   */
-
-  public getDatatype (id, callback) {
+  public getDatatype (id, callback, field) {
     let datatype;
     this.changedObjectsDatabase.transaction('r', this.changedObjectsDatabase.datatypes, async() => {
       datatype = await this.changedObjectsDatabase.datatypes.get(id);
       if (datatype != null) {
-        callback(datatype.object);
-      } else {
-        this.objectsDatabase.transaction('r', this.objectsDatabase.datatypes, async() => {
-          datatype = await this.objectsDatabase.datatypes.get(id);
-          callback(datatype.object);
-        });
+        if (field === 'metadata') {
+          callback(datatype.metadata);
+        } else if (field === 'definition') {
+          callback(datatype.definition);
+        } else if (field === 'crossReference') {
+          callback(datatype.crossReference);
+        }
       }
     });
   }
-  public getValueSet (id, callback) {
+  public getValueSet (id, callback, field) {
     let valueSet;
     this.changedObjectsDatabase.transaction('r', this.changedObjectsDatabase.valueSets, async() => {
       valueSet = await this.changedObjectsDatabase.valueSets.get(id);
       if (valueSet != null) {
-        callback(valueSet.object);
-      } else {
-        this.objectsDatabase.transaction('r', this.objectsDatabase.valueSets, async() => {
-          valueSet = await this.objectsDatabase.valueSets.get(id);
-          callback(valueSet.object);
-        });
+        if (field === 'metadata') {
+          callback(valueSet.metadata);
+        } else if (field === 'definition') {
+          callback(valueSet.definition);
+        } else if (field === 'crossReference') {
+          callback(valueSet.crossReference);
+        }
       }
     });
   }
 
-  public getSegment (id, callback) {
+  public getSegment (id, callback, field) {
     let segment;
     this.changedObjectsDatabase.transaction('r', this.changedObjectsDatabase.segments, async() => {
       segment = await this.changedObjectsDatabase.segments.get(id);
       if (segment != null) {
-        callback(segment.object);
-      } else {
-        this.objectsDatabase.transaction('r', this.objectsDatabase.segments, async() => {
-          segment = await this.objectsDatabase.segments.get(id);
-          callback(segment.object);
-        });
+        if (field === 'metadata') {
+          callback(segment.metadata);
+        } else if (field === 'definition') {
+          callback(segment.definition);
+        } else if (field === 'crossReference') {
+          callback(segment.crossReference);
+        }
       }
     });
   }
-
-  public getDatatypeMetadata (id, callback) {
-    this.getDatatype(id, function(datatype){
-      const metadataDatatype = {
-        'id': datatype.id,
-        'name': datatype.name,
-        'ext': datatype.ext,
-        'label': datatype.label,
-        'scope': datatype.scope,
-        'publicationVersion': datatype.publicationVersion,
-        'hl7Version': datatype.hl7Version,
-        'hl7Versions': datatype.hl7Versions,
-        'numberOfComponents': datatype.components.length,
-        'type': datatype.type
-      }
-       callback(metadataDatatype);
-    });
-  }
-  public getSegmentMetadata (id, callback) {
-    this.getSegment(id, function(segment){
-      const metadataSegment = {
-        'id': segment.id,
-        'name': segment.name,
-        'scope': segment.scope,
-        'hl7Version': segment.hl7Version,
-        'numberOfFields': segment.fields.length,
-        'type': segment.type
-      }
-       callback(metadataSegment);
-    });
-  }
-  public getValueSetMetadata (id, callback) {
-    this.getValueSet(id, function(valueSet){
-      const metadataValueSet = {
-        'id': valueSet.id,
-        'bindingIdentifier': valueSet.bindingIdentifier,
-        'scope': valueSet.scope,
-        'hl7Version': valueSet.hl7Version,
-        'type': valueSet.type
-      }
-       callback(metadataValueSet);
-    });
-  }
-
-  private injectDatatypes(libId) {
-    this.datatypesService.getDatatypes(libId, this.populateDatatypes.bind(this));
-  }
-  private injectValueSets(libID) {
-    this.valueSetsService.getValueSets(libID, this.populateValueSets.bind(this));
-  }
-
-  private injectSegments(libId) {
-    this.segmentsService.getSegments(libId, this.populateSegments.bind(this));
-  }
-  private injectProfileComponents(libID) {
-    this.profileComponentsService.getProfileComponents(libID, this.populateProfileComponents.bind(this));
-  }
-
-  private populateDatatypes (datatypes) {
-    datatypes.forEach(datatype => {
-      this.objectsDatabase.transaction('rw', this.objectsDatabase.datatypes, async() => {
-        await this.objectsDatabase.datatypes.put({
-          'id': datatype.id,
-          'object': datatype
-        });
-      });
-    });
-  };
-  private populateValueSets (valueSets) {
-    valueSets.forEach(valueSet => {
-      this.objectsDatabase.transaction('rw', this.objectsDatabase.valueSets, async() => {
-        await this.objectsDatabase.valueSets.put({
-          'id': valueSet.id,
-          'object': valueSet
-        });
-      });
-    });
-  };
-
-  private populateSegments (segments) {
-    segments.forEach(segment => {
-      this.objectsDatabase.transaction('rw', this.objectsDatabase.segments, async() => {
-        await this.objectsDatabase.segments.put({
-          'id': segment.id,
-          'object': segment
-        });
-      });
-    });
-  };
-  private populateProfileComponents (profileComponents) {
-    profileComponents.forEach(pc => {
-      this.objectsDatabase.transaction('rw', this.objectsDatabase.profileComponents, async() => {
-        await this.objectsDatabase.profileComponents.put({
-          'id': pc.id,
-          'object': pc
-        });
-      });
-    });
-  };
   public saveDatatype(datatype) {
     this.changedObjectsDatabase.transaction('rw', this.changedObjectsDatabase.datatypes, async() => {
-      await this.changedObjectsDatabase.datatypes.put({
-        'id': datatype.id,
-        'object': datatype
-      });
+      let savedDatatype = await this.changedObjectsDatabase.datatypes.get(datatype.id);
+      if (savedDatatype.isUndefined()) {
+        savedDatatype = new IObject();
+        savedDatatype.id = datatype.id;
+      }
+      let changesToBeSaved = false;
+      if (!datatype.metadata.isUndefined()) {
+        savedDatatype.metadata = datatype.metadata;
+        changesToBeSaved = true;
+      }
+      if (!datatype.definition.isUndefined()) {
+        savedDatatype.definition = datatype.definition;
+        changesToBeSaved = true;
+      }
+      if (!datatype.metadata.isUndefined()) {
+        savedDatatype.crossReference = datatype.crossReference;
+        changesToBeSaved = true;
+      }
+      if (changesToBeSaved) {
+        await this.changedObjectsDatabase.datatypes.put(savedDatatype);
+      }
     });
   }
   public saveSegment(segment) {
@@ -224,15 +109,28 @@ export class IndexedDbService {
     this.changedObjectsDatabase.transaction('rw', this.changedObjectsDatabase.segments, async() => {
       await this.changedObjectsDatabase.segments.put({
         'id': segment.id,
-        'object': segment
+        'metadata': segment.metadata,
+        'definition': segment.definition,
+        'crossReference': segment.crossReference
       });
     });
   }
 
-  /*public saveChangedDatatypes() {
-    this.changedObjectsDatabase.transaction('rw', this.changedObjectsDatabase.datatypes, async () => {
-      const changedDatatypes = await this.changedObjectsDatabase.datatypes.toArray();
-      this.datatypesService.saveDatatypes(changedDatatypes);
+  public persistChanges() {
+    const changedObjects = new ChangedObjects(this.igDocumentId);
+    this.changedObjectsDatabase.transaction('rw', this.changedObjectsDatabase.segments, async() => {
+      changedObjects.segments = await this.changedObjectsDatabase.segments.toArray();
     });
-  }*/
+    // call igDocumentService save method
+  }
+
+}
+
+class ChangedObjects {
+  igDocumentId?: string;
+  segments?: IObject;
+  datatypes?: IObject;
+  constructor(igDocumentId) {
+    this.igDocumentId = igDocumentId;
+  }
 }
--- client/src/app/service/indexed-db/objects-database.ts
+++ client/src/app/service/indexed-db/objects-database.ts
@@ -1,8 +1,15 @@
 import Dexie from 'dexie';
 
-interface IObject {
+export class IObject {
   id?: string;
-  object?: object;
+  metadata?: object;
+  definition?: object;
+  crossReference?: object;
+}
+
+interface ISection {
+  id?: string;
+  content?: object;
 }
 
 /*
@@ -36,19 +43,21 @@ export class ObjectsDatabase extends Dexie {
   datatypes: Dexie.Table<IObject, number>;
   valueSets: Dexie.Table<IObject, number>;
   segments: Dexie.Table<IObject, number>;
-  sections: Dexie.Table<IObject, number>;
+  sections: Dexie.Table<ISection, number>;
   profileComponents: Dexie.Table<IObject, number>;
-  profiles: Dexie.Table<IObject, number>;
+  conformanceProfiles: Dexie.Table<IObject, number>;
+  compositeProfiles: Dexie.Table<IObject, number>;
 
   constructor(name) {
     super(name);
     this.version(1).stores({
-      datatypes: '++id,object',
-      segments: '++id,object',
+      datatypes: '++id,object,object,object',
+      segments: '++id,object,object,object',
       sections: '++id,object',
-      profileComponents: '++id,object',
-      profiles: '++id,object',
-      valueSets: '++id,object'
+      profileComponents: '++id,object,object,object',
+      conformanceProfiles: '++id,object,object,object',
+      compositeProfiles: '++id,object,object,object',
+      valueSets: '++id,object,object,object'
     });
   }
 }
