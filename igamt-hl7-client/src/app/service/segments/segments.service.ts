853b2b203b126f18f3dffa4e1a9a4f7edc37cda6
Lefort, Maxence C <maxence.lefort@nist.gov>
2018-05-01 10:22:29 -0400

    populate segment service

--- igamt-hl7-client/src/app/service/segments/segments.service.ts
+++ igamt-hl7-client/src/app/service/segments/segments.service.ts
@@ -1,6 +1,5 @@
 import {Injectable} from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-import { IndexedDbService } from '../indexed-db/indexed-db.service';
 import {SegmentsIndexedDbService} from '../indexed-db/segments/segments-indexed-db.service';
 
 @Injectable()
@@ -8,8 +7,30 @@ export class SegmentsService {
   constructor(private http: HttpClientModule, private segmentsIndexedDbService: SegmentsIndexedDbService) {}
   public getSegmentMetadata(id, callback) {
     this.segmentsIndexedDbService.getSegmentMetadata(id, function(clientSegmentMetadata){
-      if (clientSegmentMetadata.isUndefined()) {
+      if (clientSegmentMetadata == null) {
         this.http.get('api/segments/' + id + '/metadata').then(function(serverSegmentMetadata){
+          callback(serverSegmentMetadata);
+        });
+      } else {
+        callback(clientSegmentMetadata);
+      }
+    });
+  }
+  public getSegmentDefinition(id, callback) {
+    this.segmentsIndexedDbService.getSegmentDefinition(id, function(clientSegmentMetadata){
+      if (clientSegmentMetadata == null) {
+        this.http.get('api/segments/' + id + '/definition').then(function(serverSegmentMetadata){
+          callback(serverSegmentMetadata);
+        });
+      } else {
+        callback(clientSegmentMetadata);
+      }
+    });
+  }
+  public getSegmentCrossReferences(id, callback) {
+    this.segmentsIndexedDbService.getSegmentCrossReference(id, function(clientSegmentMetadata){
+      if (clientSegmentMetadata == null) {
+        this.http.get('api/segments/' + id + '/crossReferences').then(function(serverSegmentMetadata){
           callback(serverSegmentMetadata);
         });
       } else {
