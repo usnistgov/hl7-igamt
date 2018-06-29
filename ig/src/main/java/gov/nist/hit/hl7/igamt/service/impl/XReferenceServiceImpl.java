9499d45a442bbf39a1034ff2e8012580cd281607
Harold Affo <affoharold@yahoo.fr>
2018-06-29 18:20:23 -0400

    update cross reference for value set

--- ig/src/main/java/gov/nist/hit/hl7/igamt/service/impl/XReferenceServiceImpl.java
+++
@@ -1,9 +0,0 @@
-package gov.nist.hit.hl7.igamt.service.impl;
-
-import gov.nist.hit.hl7.igamt.ig.service.XReferenceService;
-
-public class XReferenceServiceImpl implements XReferenceService {
-
-
-
-}
--- x-reference/src/main/java/gov/nist/hit/hl7/igamt/xreference/util/XReferenceUtil.java
+++ x-reference/src/main/java/gov/nist/hit/hl7/igamt/xreference/util/XReferenceUtil.java
@@ -12,6 +12,8 @@
 package gov.nist.hit.hl7.igamt.xreference.util;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 import org.bson.types.ObjectId;
@@ -26,24 +28,38 @@ import com.mongodb.BasicDBObject;
 public class XReferenceUtil {
 
   /**
-   * TODO: Do this from mongo query ?? Remove non matching element and set absolute path for
-   * matching element
+   * remove unecessary fiels, restructure the references
    * 
    * @param results
    * @return
    */
-  public static List<BasicDBObject> filterChildrenBySegmentId(
+  public static List<BasicDBObject> processSegmentReferences(
       List<BasicDBObject> conformanceProfiles, String segmentId) {
     for (BasicDBObject conformanceProfile : conformanceProfiles) {
-      List<BasicDBObject> where = filterChildrenBySegmentId(conformanceProfile, segmentId);
+      List<BasicDBObject> children = processSegmentReferences(conformanceProfile, segmentId);
       conformanceProfile.remove("children");
       conformanceProfile.remove("_class");
       conformanceProfile.remove("postDef");
       conformanceProfile.remove("preDef");
       conformanceProfile.remove("comment");
-      conformanceProfile.append("children", where);
+      conformanceProfile.append("children", children);
     }
     return conformanceProfiles;
+  }
+
+
+
+  public static List<BasicDBObject> sortDSCByVersion(List<BasicDBObject> objects) {
+    Collections.sort(objects, new Comparator<BasicDBObject>() {
+      @Override
+      public int compare(BasicDBObject o1, BasicDBObject o2) {
+        // TODO Auto-generated method stub
+        int v1 = ((BasicDBObject) o1.get("_id")).getInt("version");
+        int v2 = ((BasicDBObject) o2.get("_id")).getInt("version");
+        return v2 - v1;
+      }
+    });
+    return objects;
   }
 
   /**
@@ -51,7 +67,7 @@ public class XReferenceUtil {
    * @param object
    * @return
    */
-  private static String getName(BasicDBObject object) {
+  public static String getName(BasicDBObject object) {
     String ext = object.getString("ext") != null && !object.getString("ext").equals("")
         ? object.getString("ext") : null;
     return ext != null ? object.getString("name") + "_" + ext : object.getString("name");
@@ -76,54 +92,13 @@ public class XReferenceUtil {
   }
 
 
-  public static List<BasicDBObject> filterReferencesByValueSetId(List<BasicDBObject> references,
-      String valueSetId) {
-    for (BasicDBObject reference : references) {
-      filterReferencesByValueSetId(reference, valueSetId);
-    }
-    return references;
-  }
-
-
-  public static BasicDBObject filterReferencesByValueSetId(BasicDBObject referenceObject,
-      String valueSetId) {
-    List<BasicDBObject> children =
-        (List<BasicDBObject>) ((BasicDBObject) referenceObject.get("binding")).get("children");
-    List<BasicDBObject> tmp = new ArrayList<BasicDBObject>();
-    for (BasicDBObject child : children) {
-      List<BasicDBObject> valuesetBindings = (List<BasicDBObject>) child.get("valuesetBindings");
-      List<BasicDBObject> tmpValuesetBinding = new ArrayList<BasicDBObject>();
-      for (BasicDBObject valuesetBinding : valuesetBindings) {
-        if (valuesetBinding.get("valuesetId").equals(valueSetId)) {
-          valuesetBinding.remove("valuesetId"); // no need of extra information
-          tmpValuesetBinding.add(valuesetBinding);
-        }
-      }
-      if (!tmpValuesetBinding.isEmpty()) {
-        child.remove("valuesetBindings");
-        child.append("valuesetBindings", tmpValuesetBinding);
-        tmp.add(child);
-      }
-    }
-    referenceObject.remove("_class");
-    referenceObject.remove("children");
-    referenceObject.remove("preDef");
-    referenceObject.remove("postDef");
-    referenceObject.remove("description");
-    referenceObject.remove("comment");
-    referenceObject.remove("binding");
-    referenceObject.append("children", tmp);
-    return referenceObject;
-  }
-
-
 
   /**
    * 
    * @param datatype
    * @return
    */
-  public static BasicDBObject filterDatatype(BasicDBObject datatype) {
+  public static BasicDBObject processDatatype(BasicDBObject datatype) {
     List<BasicDBObject> children = (List<BasicDBObject>) datatype.get("children");
     for (BasicDBObject child : children) {
       child.append("path", getName(datatype) + "." + child.get("position"));
@@ -142,7 +117,7 @@ public class XReferenceUtil {
    * @param segments
    * @return
    */
-  public static List<BasicDBObject> filterSegments(List<BasicDBObject> segments) {
+  public static List<BasicDBObject> processSegments(List<BasicDBObject> segments) {
     for (BasicDBObject segment : segments) {
       filterSegment(segment);
     }
@@ -155,9 +130,9 @@ public class XReferenceUtil {
    * @param datatypes
    * @return
    */
-  public static List<BasicDBObject> filterDatatypes(List<BasicDBObject> datatypes) {
+  public static List<BasicDBObject> processDatatypes(List<BasicDBObject> datatypes) {
     for (BasicDBObject segment : datatypes) {
-      filterDatatype(segment);
+      processDatatype(segment);
     }
     return datatypes;
   }
@@ -168,7 +143,7 @@ public class XReferenceUtil {
    * @param segmentId
    * @return
    */
-  public static List<BasicDBObject> filterChildrenBySegmentId(BasicDBObject conformanceProfile,
+  public static List<BasicDBObject> processSegmentReferences(BasicDBObject conformanceProfile,
       String segmentId) {
     List<BasicDBObject> tmp = new ArrayList<BasicDBObject>();
     List<BasicDBObject> children = (List<BasicDBObject>) conformanceProfile.get("children");
@@ -177,12 +152,12 @@ public class XReferenceUtil {
       for (BasicDBObject child : children) {
         child.append("path", child.get("position") + "");
         if (child.get("type").equals("SEGMENTREF")) {
-          BasicDBObject res = filterSegmentRefBySegmentId(child, segmentId);
+          BasicDBObject res = processSegmentRef(child, segmentId);
           if (res != null) {
             tmp.add(res);
           }
         } else {
-          List<BasicDBObject> groupResults = filterGroupBySegmentId(child, segmentId);
+          List<BasicDBObject> groupResults = processGroup(child, segmentId);
           if (groupResults != null && !groupResults.isEmpty()) {
             tmp.addAll(groupResults);
           }
@@ -198,8 +173,7 @@ public class XReferenceUtil {
    * @param segmentId
    * @return
    */
-  public static BasicDBObject filterSegmentRefBySegmentId(BasicDBObject segmentRef,
-      String segmentId) {
+  public static BasicDBObject processSegmentRef(BasicDBObject segmentRef, String segmentId) {
     BasicDBObject result = matchSegment(segmentRef, segmentId) ? segmentRef : null;
     if (result != null) {
       result.remove("_class");
@@ -216,19 +190,19 @@ public class XReferenceUtil {
    * @param segmentId
    * @return
    */
-  public static List<BasicDBObject> filterGroupBySegmentId(BasicDBObject group, String segmentId) {
+  public static List<BasicDBObject> processGroup(BasicDBObject group, String segmentId) {
     List<BasicDBObject> tmp = new ArrayList<BasicDBObject>();
     List<BasicDBObject> children = (List<BasicDBObject>) group.get("children");
     if (children != null) {
       for (BasicDBObject child : children) {
         child.append("path", group.get("path") + "." + child.getInt("position"));
         if (child.get("type").equals("SEGMENTREF")) {
-          BasicDBObject res = filterSegmentRefBySegmentId(child, segmentId);
+          BasicDBObject res = processSegmentRef(child, segmentId);
           if (res != null) {
             tmp.add(res);
           }
         } else {
-          List<BasicDBObject> groupResults = filterGroupBySegmentId(child, segmentId);
+          List<BasicDBObject> groupResults = processGroup(child, segmentId);
           if (groupResults != null && !groupResults.isEmpty()) {
             tmp.addAll(groupResults);
           }
