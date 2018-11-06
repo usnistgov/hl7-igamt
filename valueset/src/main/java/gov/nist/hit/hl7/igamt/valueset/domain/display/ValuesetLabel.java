6165f3e1bf6a86db309988df3ebb1182ff072afc
Woo, Jungyub (Assoc) <jungyub.woo@nist.gov>
2018-11-06 14:30:08 -0500

    pushed for final

---
+++ valueset/src/main/java/gov/nist/hit/hl7/igamt/valueset/domain/display/ValuesetLabel.java
@@ -0,0 +1,70 @@
+/**
+ * This software was developed at the National Institute of Standards and Technology by employees of
+ * the Federal Government in the course of their official duties. Pursuant to title 17 Section 105
+ * of the United States Code this software is not subject to copyright protection and is in the
+ * public domain. This is an experimental system. NIST assumes no responsibility whatsoever for its
+ * use by other parties, and makes no guarantees, expressed or implied, about its quality,
+ * reliability, or any other characteristic. We would appreciate acknowledgement if the software is
+ * used. This software can be redistributed and/or modified freely provided that any derivative
+ * works bear some notice that they are derived from it, and any modified versions bear some notice
+ * that they have been modified.
+ */
+package gov.nist.hit.hl7.igamt.valueset.domain.display;
+
+import gov.nist.hit.hl7.igamt.common.base.domain.CompositeKey;
+import gov.nist.hit.hl7.igamt.common.base.domain.Scope;
+
+/**
+ * @author jungyubw
+ *
+ */
+public class ValuesetLabel {
+
+  private CompositeKey id;
+  private Scope scope;
+  private String label;
+  private String name;
+  private String version;
+
+  public CompositeKey getId() {
+    return id;
+  }
+
+  public void setId(CompositeKey id) {
+    this.id = id;
+  }
+
+  public Scope getScope() {
+    return scope;
+  }
+
+  public void setScope(Scope scope) {
+    this.scope = scope;
+  }
+
+  public String getLabel() {
+    return label;
+  }
+
+  public void setLabel(String label) {
+    this.label = label;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getVersion() {
+    return version;
+  }
+
+  public void setVersion(String version) {
+    this.version = version;
+  }
+
+
+}
