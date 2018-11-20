11177986da985ed3cd345225ed71df3891ea97da
Tamri, Hossam (IntlAssoc) <hossam.tamri@nist.gov>
2018-11-20 12:03:32 -0500

    Update tree for selection restrictions

--- igamt-hl7-client/src/app/common/cs-segment-tree/cs-segment-tree.component.ts
+++ igamt-hl7-client/src/app/common/cs-segment-tree/cs-segment-tree.component.ts
@@ -2,6 +2,16 @@ import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {TreeNode} from 'primeng/api';
 import {GeneralConfigurationService} from '../../service/general-configuration/general-configuration.service';
 
+
+export interface TreeRestrictions {
+  primitive: boolean;
+  datatypes: string[];
+  repeat: boolean;
+  usages:  string[];
+  types: string[];
+}
+
+
 @Component({
   selector: 'app-cs-segment-tree',
   templateUrl: './cs-segment-tree.component.html',
@@ -10,14 +20,21 @@ import {GeneralConfigurationService} from '../../service/general-configuration/g
 export class CsSegmentTreeComponent implements OnInit {
 
   _tree: TreeNode[];
+  _restrictions: TreeRestrictions;
   selectedNode: TreeNode;
   @Output() select: EventEmitter<any> = new EventEmitter<any>();
-  @Input()  selectComplex = false;
+
 
   constructor(private configService: GeneralConfigurationService) {}
 
+  @Input() set restrictions(restrictions: TreeRestrictions){
+    this._restrictions = restrictions;
+    if (this._tree && this._tree.length > 0) {
+      this.evaluate(null, this._tree[0], restrictions);
+    }
+  }
+
   @Input() set tree(tree: any){
-    console.log(tree);
     const node: TreeNode = {
       label: tree.label,
       type: 'root',
@@ -29,29 +46,72 @@ export class CsSegmentTreeComponent implements OnInit {
       },
       children : this.configService.arraySortByPosition(tree.structure)
     };
-
-    if (!this.selectComplex) {
-      this.disableComplex(node);
+    if (this._restrictions) {
+      this.evaluate(null, node, this._restrictions);
     }
 
     this._tree = [node];
-    console.log(this._tree);
   }
 
-  disableComplex(node: TreeNode) {
+  evaluate(parent: TreeNode, node: TreeNode, restrictions: TreeRestrictions) {
+    let filter = true;
+    node.parent = parent;
+    node.data.container = false;
+    const node_has_children = !(!node.children || node.children.length === 0);
+    const node_has_datatype = node.data.datatypeLabel &&  node.data.datatypeLabel.name && node.data.datatypeLabel.name !== '';
+    const node_datatype = node_has_datatype ? node.data.datatypeLabel.name.toUpperCase() : '';
+    const node_type = node.data.type.toUpperCase();
+    const node_repeats = node.data.max ? (node.data.max === '*' || +node.data.max > 1) : false;
+    const node_usage = node.data.usage ? node.data.usage.toUpperCase() : '';
+
+    // --- Primitive Filter
+    if (restrictions.primitive !== undefined) {
+      filter = filter && (!restrictions.primitive || !node_has_children) && (restrictions.primitive || node_has_children);
+    }
+
+    // --- Datatypes Filter
+    if (restrictions.datatypes !== undefined && restrictions.datatypes.length > 0) {
+      filter = filter && (!node_has_datatype || restrictions.datatypes.indexOf(node_datatype) !== -1);
+    }
+
+    // --- Elm type Filter
+    if (restrictions.types !== undefined && restrictions.types.length > 0) {
+      filter = filter && (restrictions.types.indexOf(node_type) !== -1);
+    }
+
+    // --- Repeat Filter
+    if (restrictions.repeat !== undefined) {
+      filter = filter && (!restrictions.repeat || node_repeats) && (restrictions.repeat || !node_repeats);
+    }
+
+    // --- Usage Filter
+    if (restrictions.usages !== undefined && restrictions.usages.length > 0 && node.data.usage) {
+      filter = filter && (restrictions.usages.indexOf(node_usage) !== -1);
+    }
+    node.selectable = filter;
+    if (filter && parent) {
+      parent.data.container = true;
+    }
     if (node.children && node.children.length > 0) {
-      node.selectable = false;
-      for (const child of node.children){
-        this.disableComplex(child);
+      for (const child of node.children) {
+        this.evaluate(node, child, restrictions);
       }
-    } else {
-      node.selectable = true;
     }
   }
 
+  markParent(node: TreeNode, b: boolean) {
+    if (node.parent) {
+      console.log("PARENT");
+      node.parent.data.container = b;
+      this.markParent(node.parent, b);
+    }
+  }
+
+
+
   selected(event) {
     this.selectedNode = event.node;
-    this.select.emit(this.processPath(event.node));
+    this.select.emit([event.node, this.processPath(event.node)]);
   }
 
   processPath(n: TreeNode) {
