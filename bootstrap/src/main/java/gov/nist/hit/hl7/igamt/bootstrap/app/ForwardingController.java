339baa7fbaa74984606bb5561eef2096b45cbb65
El Ouakili, Abdelghani (IntlAssoc) <abdelghani.elouakili@nist.net>
2019-04-18 10:52:48 -0400

    email configuration

---
+++ bootstrap/src/main/java/gov/nist/hit/hl7/igamt/bootstrap/app/ForwardingController.java
@@ -0,0 +1,13 @@
+package gov.nist.hit.hl7.igamt.bootstrap.app;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+class ForwardingController { 
+
+    @RequestMapping(value = "/**/{path:[^.]*}")       
+    public String redirect() {
+        return "forward:/";
+    }
+}
