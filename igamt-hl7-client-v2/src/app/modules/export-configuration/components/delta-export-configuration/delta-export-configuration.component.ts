25aa521feb38a7cbce5933f36f90a2b74faff461
Ismail Mellouli <ismail.mellouli@nist.gov>
2019-11-01 12:16:23 -0400

    Datatype delta export

--- igamt-hl7-client-v2/src/app/modules/export-configuration/components/delta-export-configuration/delta-export-configuration.component.ts
+++ igamt-hl7-client-v2/src/app/modules/export-configuration/components/delta-export-configuration/delta-export-configuration.component.ts
@@ -17,6 +17,51 @@ export class DeltaExportConfigurationComponent implements OnInit {
       value: 'HIDE',
     },
   ];
+  public defaultColors = [
+    [
+      '#ff1744',
+      '#d500f9',
+      '#3d5afe',
+      '#00b0ff',
+      '#00e5ff',
+      '#1de9b6',
+      '#00e676',
+      '#76ff03',
+      '#ffea00',
+      '#ff9100',
+      '#4e342e',
+      '#37474f'
+    ],
+    [
+      '#ff8a80',
+      '#ea80fc',
+      '#8c9eff',
+      '#80d8ff',
+      '#84ffff',
+      '#a7ffeb',
+      '#b9f6ca',
+      '#ccff90',
+      '#ffff8d',
+      '#ffd180',
+      '#6d4c41',
+      '#546e7a'
+    ],
+    [
+      '#e57373',
+      '#ba68c8',
+      '#7986cb',
+      '#4fc3f7',
+      '#4dd0e1',
+      '#4db6ac',
+      '#81c784',
+      '#aed581',
+      '#fff176',
+      '#ffb74d',
+      '#a1887f',
+      '#90a4ae'
+    ]
+
+  ];
 
   @Input()
   active: any;
@@ -24,12 +69,41 @@ export class DeltaExportConfigurationComponent implements OnInit {
   config: any;
   @Output()
   updateDelta: EventEmitter<any> = new EventEmitter<any>();
+
+  colorOpened = false;
+  openedFor;
   constructor() { }
 
   ngOnInit() {
   }
-  triggerChange() {
+  triggerChange(ev) {
+    console.log(ev);
     this.updateDelta.emit({ active: this.active, config: this.config });
+  }
+  toggleColors(target) {
+    console.log(target)
+    if (target !== this.openedFor) {
+      this.openedFor = target;
+      this.colorOpened = true;
+    } else {
+      this.openedFor = null;
+      this.colorOpened = false;
+    }
+  }
+  selectColor(target, color) {
+    this.openedFor = null;
+    this.colorOpened = false;
+    switch (target) {
+      case 'UPDATED':
+        this.config.colors.UPDATED = color;
+        break;
+      case 'ADDED':
+        this.config.colors.ADDED = color;
+        break;
+      case 'DELETED':
+        this.config.colors.DELETED = color;
+        break;
+    }
   }
 
 }
