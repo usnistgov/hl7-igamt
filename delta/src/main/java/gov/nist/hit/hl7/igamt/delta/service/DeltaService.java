ab9e041d0bf7e5f7aca72be5836be6ec259b0903
Ismail Mellouli <ismail.mellouli@nist.gov>
2020-10-09 14:17:43 -0400

    #1156: Derived IG: Co-constraint Deltas

--- delta/src/main/java/gov/nist/hit/hl7/igamt/delta/service/DeltaService.java
+++ delta/src/main/java/gov/nist/hit/hl7/igamt/delta/service/DeltaService.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import gov.nist.diff.domain.DeltaAction;
 import gov.nist.hit.hl7.igamt.coconstraints.exception.CoConstraintGroupNotFoundException;
+import gov.nist.hit.hl7.igamt.coconstraints.model.CoConstraintBinding;
 import gov.nist.hit.hl7.igamt.common.base.domain.RealKey;
 import gov.nist.hit.hl7.igamt.common.base.domain.Resource;
 import gov.nist.hit.hl7.igamt.common.base.domain.Type;
@@ -23,7 +24,7 @@ public interface DeltaService {
   public IGDisplayInfo delta(Ig ig, Ig origin) throws IGDeltaException;
   public ResourceDelta delta(Type type, Resource resource);
   public ValuesetDelta valuesetDelta(Valueset valueset);
-  DeltaAction summarize(List<StructureDelta> deltaStructure, List<ConformanceStatementDelta> cfs);
+  DeltaAction summarize(List<StructureDelta> deltaStructure, List<ConformanceStatementDelta> cfs,  List<CoConstraintBinding> coConstraintBindings);
 
 
 }
