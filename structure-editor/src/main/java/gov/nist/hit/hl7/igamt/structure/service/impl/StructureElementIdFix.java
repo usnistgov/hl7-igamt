72351723693c2b9c647414694889795e35e11f1d
Tamri, Hossam (IntlAssoc) <hossam.tamri@nist.gov>
2020-11-03 12:49:35 -0500

    add link to structure

--- structure-editor/src/main/java/gov/nist/hit/hl7/igamt/structure/service/impl/StructureElementIdFix.java
+++ structure-editor/src/main/java/gov/nist/hit/hl7/igamt/structure/service/impl/StructureElementIdFix.java
@@ -9,14 +9,14 @@ import gov.nist.hit.hl7.igamt.datatype.service.DatatypeService;
 import gov.nist.hit.hl7.igamt.segment.repository.SegmentRepository;
 import gov.nist.hit.hl7.igamt.segment.service.SegmentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
-import javax.annotation.PostConstruct;
+
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-@Service
+//@Service
 public class StructureElementIdFix {
     @Autowired
     MessageStructureRepository messageStructureRepository;
@@ -29,20 +29,40 @@ public class StructureElementIdFix {
     @Autowired
     ConformanceProfileRepository conformanceProfileRepository;
 
-    @PostConstruct
+//    @PostConstruct
     void fix() {
-        System.out.println("[STRUCTURE CUSTOM]");
         List<MessageStructure> messageStructures = this.getAllAffectedCustomStructures();
         System.out.println("[STRUCTURE CUSTOM] " + messageStructures.size());
         messageStructures.forEach((x) -> {
-            System.out.println(x.getStructID() + " - " + x.getParticipants());
+            System.out.println(x.getStructID() + " - " + x.getParticipants() + " - " + x.getId());
+            System.out.println(this.listAllAffectedIds(x.getChildren()));
         });
-        System.out.println("[CP CUSTOM]");
+
         List<ConformanceProfile> conformanceProfiles = this.getAllAffectedConformanceProfiles();
         System.out.println("[CP CUSTOM] " + conformanceProfiles.size());
         conformanceProfiles.forEach((x) -> {
             System.out.println(x.getStructID() + " - " + x.getUsername() + " - " + x.getId());
+            System.out.println(this.listAllAffectedIds(x.getChildren()));
         });
+    }
+
+    Set<String> listAllAffectedIds(Set<SegmentRefOrGroup> segmentRefOrGroups) {
+        Set<String> ids = new HashSet<>();
+        for(SegmentRefOrGroup segmentRefOrGroup: segmentRefOrGroups) {
+            if(segmentRefOrGroup instanceof Group) {
+                Group group = (Group) segmentRefOrGroup;
+                if(!this.isIdValid(group)) {
+                    ids.add(group.getId());
+                }
+                ids.addAll(this.listAllAffectedIds(group.getChildren()));
+            } else if(segmentRefOrGroup instanceof SegmentRef) {
+                SegmentRef segmentRef = (SegmentRef) segmentRefOrGroup;
+                if(!this.isIdValid(segmentRef)) {
+                    ids.add(segmentRef.getId());
+                }
+            }
+        }
+        return ids;
     }
 
     List<MessageStructure> getAllAffectedCustomStructures() {
