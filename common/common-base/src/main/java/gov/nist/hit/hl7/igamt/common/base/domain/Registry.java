5af513ed12beed7f2d0117186c350553efb117a3
El Ouakili, Abdelghani (IntlAssoc) <abdelghani.elouakili@nist.net>
2021-03-09 12:41:19 -0500

    Merged

--- common/common-base/src/main/java/gov/nist/hit/hl7/igamt/common/base/domain/Registry.java
+++ common/common-base/src/main/java/gov/nist/hit/hl7/igamt/common/base/domain/Registry.java
@@ -49,4 +49,12 @@ public class Registry implements Serializable{
 	  return null;
   }
 
+  /**
+   * @return
+   */
+  public Type getType() {
+    // TODO Auto-generated method stub
+    return type;
+  }
+
 }
