da4dd3ec7d88473cfea88a4c1b8c5a05ca6727a0
El Ouakili, Abdelghani (IntlAssoc) <abdelghani.elouakili@nist.net>
2021-07-10 23:57:55 -0400

    release v1.5.4-beta

--- bootstrap/src/main/java/gov/nist/hit/hl7/igamt/bootstrap/app/BootstrapApplication.java
+++ bootstrap/src/main/java/gov/nist/hit/hl7/igamt/bootstrap/app/BootstrapApplication.java
@@ -341,7 +341,7 @@ public class BootstrapApplication implements CommandLineRunner {
     tableFixes.removeSegmentsDuplicatedBinding();
   }
   
- // @PostConstruct
+  //@PostConstruct
   void generateDefaultExportConfig() {
       exportConfigurationRepository.deleteByType(ExportType.IGDOCUMENT);
       ExportConfiguration basicExportConfiguration = ExportConfiguration.getBasicExportConfiguration(false, ExportType.IGDOCUMENT);
@@ -355,7 +355,7 @@ public class BootstrapApplication implements CommandLineRunner {
   }
 
   
- // @PostConstruct
+ //@PostConstruct
   void generateDiffrentialExportConfig() {
     exportConfigurationRepository.deleteByType(ExportType.DIFFERENTIAL);
     ExportConfiguration basicExportConfiguration = ExportConfiguration.getBasicExportConfiguration(true, ExportType.DIFFERENTIAL);
@@ -1031,7 +1031,7 @@ public class BootstrapApplication implements CommandLineRunner {
     codeFixer.fixFromCSV();
   }
   
-  @PostConstruct
+  //@PostConstruct
   void generateDefaultFontConfigForAll(){
     List<ExportConfiguration> allConfigs= exportConfigurationRepository.findAll();
     ExportFontConfiguration fontConfig = ExportFontConfiguration.getDefault();
