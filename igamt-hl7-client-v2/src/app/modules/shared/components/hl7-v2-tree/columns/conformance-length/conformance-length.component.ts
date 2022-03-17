389b3acc6caec6090f9a8767bcaaef93a83d5cd1
Hossam Tamri <hossam.tamri@nist.gov>
2022-03-17 15:32:17 -0400

    HL7 v2 Tree fix Length / ConfLength / ConstantValue

--- igamt-hl7-client-v2/src/app/modules/shared/components/hl7-v2-tree/columns/conformance-length/conformance-length.component.ts
+++ igamt-hl7-client-v2/src/app/modules/shared/components/hl7-v2-tree/columns/conformance-length/conformance-length.component.ts
@@ -1,26 +1,31 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { MatDialog } from '@angular/material';
 import { LengthType } from 'src/app/modules/shared/constants/length-type.enum';
 import { IChange } from 'src/app/modules/shared/models/save-change';
 import { ChangeType, PropertyType } from '../../../../models/save-change';
+import { IStringValue } from '../../hl7-v2-tree.component';
 import { HL7v2TreeColumnComponent } from '../hl7-v2-tree-column.component';
-import { ILengthAndConfLength } from '../length/length.component';
 
 @Component({
   selector: 'app-conformance-length',
   templateUrl: './conformance-length.component.html',
   styleUrls: ['./conformance-length.component.scss'],
 })
-export class ConformanceLengthComponent extends HL7v2TreeColumnComponent<ILengthAndConfLength> implements OnInit {
+export class ConformanceLengthComponent extends HL7v2TreeColumnComponent<IStringValue> implements OnInit {
 
-  val: ILengthAndConfLength;
+  lengthTypes = LengthType;
+
+  val: IStringValue;
   @Output()
   updateLengthType: EventEmitter<LengthType>;
+  @Input()
+  lengthType: LengthType;
+  @Input()
+  leaf: boolean;
 
-  onInitValue(value: ILengthAndConfLength): void {
+  onInitValue(value: IStringValue): void {
     this.val = {
       ...value,
-      confLength: value && value.confLength ? value.confLength : undefined,
     };
   }
 
@@ -33,20 +38,21 @@ export class ConformanceLengthComponent extends HL7v2TreeColumnComponent<ILength
   }
 
   confLengthChange(value: string) {
-    this.onChange<string>(this.getInputValue().confLength + '', value + '', PropertyType.CONFLENGTH, ChangeType.UPDATE);
+    this.onChange<string>(this.getInputValue().value + '', value + '', PropertyType.CONFLENGTH, ChangeType.UPDATE);
   }
 
   clear() {
-    this.onChange<string>(this.getInputValue().lengthType, LengthType.Length, PropertyType.LENGTHTYPE, ChangeType.UPDATE);
-    this.updateLengthType.emit(LengthType.Length);
+    this.onChange<string>(this.lengthType, LengthType.UNSET, PropertyType.LENGTHTYPE, ChangeType.UPDATE);
+    this.updateLengthType.emit(LengthType.UNSET);
+  }
+
+  set() {
+    this.onChange<string>(this.lengthType, LengthType.ConfLength, PropertyType.LENGTHTYPE, ChangeType.UPDATE);
+    this.updateLengthType.emit(LengthType.ConfLength);
   }
 
   isActualChange<X>(change: IChange<X>): boolean {
     return change.propertyValue !== change.oldPropertyValue;
-  }
-
-  active() {
-    return this.val.lengthType === LengthType.ConfLength;
   }
 
   ngOnInit() {
--- igamt-hl7-client-v2/src/app/modules/shared/components/hl7-v2-tree/columns/constant-value/constant-value.component.ts
+++ igamt-hl7-client-v2/src/app/modules/shared/components/hl7-v2-tree/columns/constant-value/constant-value.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material';
 import { IChange } from 'src/app/modules/shared/models/save-change';
 import { ChangeType, PropertyType } from '../../../../models/save-change';
@@ -15,6 +15,8 @@ export class ConstantValueComponent extends HL7v2TreeColumnComponent<IStringValu
   constant: IStringValue;
   edit: boolean;
   tmp: string;
+  @Input()
+  leaf: boolean;
 
   constructor(protected dialog: MatDialog) {
     super([PropertyType.CONSTANTVALUE], dialog);
@@ -39,6 +41,11 @@ export class ConstantValueComponent extends HL7v2TreeColumnComponent<IStringValu
       this.tmp = this.constant.value;
     }
     this.edit = !this.edit;
+  }
+
+  clear() {
+    this.constant.value = '';
+    this.onChange(this.getInputValue().value, this.constant.value, PropertyType.CONSTANTVALUE, ChangeType.UPDATE);
   }
 
   ngOnInit() {
