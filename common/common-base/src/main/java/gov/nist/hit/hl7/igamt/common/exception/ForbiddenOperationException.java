5fd72362767d660767f957c60587e281433c69d5
ABDELGHANI EL OUAKILI <abdelghani.el-ouakili@nist.gov>
2022-07-15 11:53:03 -0400

    Fix clone and refactoring issues

---
+++ common/common-base/src/main/java/gov/nist/hit/hl7/igamt/common/exception/ForbiddenOperationException.java
@@ -0,0 +1,12 @@
+package gov.nist.hit.hl7.igamt.common.exception; 
+ 
+public class ForbiddenOperationException extends Exception{ 
+ 
+ 
+	  private static final long serialVersionUID = 1L; 
+ 
+	    public ForbiddenOperationException(String message) { 
+	        super(message); 
+	    } 
+ 
+} 
