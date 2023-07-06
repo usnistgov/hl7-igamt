6e492d6944635c30a41eb3cb18c90cc2b0fdd64e
Jungyub Woo <jungyub.woo@nist.gov>
2023-07-06 14:54:13 -0400

    fix

--- web-app/src/main/java/gov/nist/hit/hl7/igamt/web/app/ig/IGDocumentController.java
+++ web-app/src/main/java/gov/nist/hit/hl7/igamt/web/app/ig/IGDocumentController.java
@@ -1400,131 +1400,14 @@ public class IGDocumentController extends BaseController {
 	@PreAuthorize("AccessResource('IGDOCUMENT', #id, WRITE) && ConcurrentSync('IGDOCUMENT', #id, ALLOW_SYNC_STRICT)")
 	public ResponseMessage<AddResourceResponse> addValuesetFromCSV(@PathVariable("id") String id,
 			@RequestParam("file") MultipartFile csvFile, Authentication authentication) throws ImportValueSetException, IGNotFoundException, ForbiddenOperationException {
-		CSVReader reader = null;
-		Ig ig = findIgById(id);
-		//    commonService.checkRight(authentication, ig.getCurrentAuthor(), ig.getUsername());
-
-		if (!csvFile.isEmpty()) {
-			String[] row = null;
-			try {
-				reader = new CSVReader(new FileReader(this.multipartToFile(csvFile, "CSVFile")));
-				
-
-				Valueset newVS = null;
-				while ((row = reader.readNext()) != null) {
-							switch (row[0]) {
-							case "Mapping Identifier":
-								if(newVS != null) {
-									newVS.getDomainInfo().setScope(Scope.USER);
-									newVS.setUsername(ig.getUsername());
-									newVS.setCurrentAuthor(ig.getCurrentAuthor());
-									newVS.setSharedUsers(ig.getSharedUsers());
-									newVS.setSharePermission(ig.getSharePermission());
-									newVS.setDocumentInfo(new DocumentInfo(ig.getId(), DocumentType.IGDOCUMENT));
-									newVS.setId(new ObjectId().toString());
-									newVS = this.valuesetService.save(newVS);
-									ig.getValueSetRegistry().getChildren()
-									.add(new Link(newVS.getId(), newVS.getDomainInfo(), ig.getValueSetRegistry().getChildren().size() + 1));
-									ig = igService.save(ig);
-								}
-								newVS = new Valueset();
-								DomainInfo domainInfo = new DomainInfo();
-								domainInfo.setScope(Scope.USER);
-								newVS.setDomainInfo(domainInfo);
-								newVS.setSourceType(SourceType.INTERNAL);
-								newVS.setBindingIdentifier(row[1]);
-								break;
-							case "Name":
-								if(newVS != null) newVS.setName(row[1]);
-								break;
-							case "Description":
-								if(newVS != null) newVS.setDescription(row[1]);
-								break;
-							case "OID":
-								if(newVS != null) newVS.setOid(row[1]);
-								break;
-							case "Version":
-								if(newVS != null) newVS.getDomainInfo().setVersion(row[1]);
-								break;
-							case "Extensibility":
-								if(newVS != null) newVS.setExtensibility(Extensibility.valueOf(row[1]));
-								break;
-							case "Stability":
-								if(newVS != null) newVS.setStability(Stability.valueOf(row[1]));
-								break;
-							case "Content Definition":
-								if(newVS != null) newVS.setContentDefinition(ContentDefinition.valueOf(row[1]));
-								break;
-							case "Comment":
-								if(newVS != null) newVS.setComment(row[1]);
-								break;
-							default:
-								if(newVS != null) {
-									if(row[0] != null && !row[0].replaceAll("\\s","").equals("") && !row[0].equals("Value")
-											&& row[1] != null && !row[1].replaceAll("\\s","").equals("")
-											&& row[2] != null && !row[2].replaceAll("\\s","").equals("")
-											&& row[3] != null && !row[3].replaceAll("\\s","").equals("")) {
-										Code code = new Code();
-										code.setValue(row[0]);
-										code.setDescription(row[1]);
-										code.setCodeSystem(row[2]);
-										code.setUsage(CodeUsage.valueOf(row[3]));
-										code.setComments(row[4]);
-
-										if (code.getCodeSystem() != null && !code.getCodeSystem().isEmpty())
-											newVS.getCodeSystems().add(code.getCodeSystem());
-										if (code.getValue() != null && !code.getValue().isEmpty()) {
-											newVS.getCodes().add(code);
-										}
-									}	
-								}
-								
-									
-							}
-						}
-
-				reader.close();
-				
-				
-				if(newVS != null) {
-					newVS.getDomainInfo().setScope(Scope.USER);
-					newVS.setUsername(ig.getUsername());
-					newVS.setCurrentAuthor(ig.getCurrentAuthor());
-					newVS.setSharedUsers(ig.getSharedUsers());
-					newVS.setSharePermission(ig.getSharePermission());
-					newVS.setDocumentInfo(new DocumentInfo(ig.getId(), DocumentType.IGDOCUMENT));
-					newVS.setId(new ObjectId().toString());
-					newVS = this.valuesetService.save(newVS);
-
-
-
-					ig.getValueSetRegistry().getChildren()
-					.add(new Link(newVS.getId(), newVS.getDomainInfo(), ig.getValueSetRegistry().getChildren().size() + 1));
-					ig = igService.save(ig);
-					AddResourceResponse response = new AddResourceResponse();
-					response.setId(newVS.getId());
-					response.setReg(ig.getValueSetRegistry());
-					response.setDisplay(displayInfoService.convertValueSet(newVS));
-					return new ResponseMessage<AddResourceResponse>(Status.SUCCESS, "", "Value Set clone Success", newVS.getId(), false,
-							newVS.getUpdateDate(), response);	
-				} else throw new ImportValueSetException("No Valueset info");
-				
-				
-			} catch (Exception e) {
-				if(row != null)
-				System.out.println(row[0]);
-				throw new ImportValueSetException(e.getLocalizedMessage());
-			}
-		}else {
-			throw new ImportValueSetException("File is Empty");
-		}
-	}
-
-	public File multipartToFile(MultipartFile multipart, String fileName)
-			throws IllegalStateException, IOException {
-		File convFile = new File(System.getProperty("java.io.tmpdir") + "/" + fileName);
-		multipart.transferTo(convFile);
-		return convFile;
+		
+		Valueset newVS = this.igService.importValuesetsFromCSV(id, csvFile);
+		AddResourceResponse response = new AddResourceResponse();
+		response.setId(newVS.getId());
+		response.setReg(findIgById(id).getValueSetRegistry());
+		response.setDisplay(displayInfoService.convertValueSet(newVS));
+		return new ResponseMessage<AddResourceResponse>(Status.SUCCESS, "", "Value Set clone Success", newVS.getId(), false,
+				newVS.getUpdateDate(), response);
 	}
 
 	@RequestMapping(value = "/api/igdocuments/{id}/grand", method = RequestMethod.GET, produces = {"application/json"})
